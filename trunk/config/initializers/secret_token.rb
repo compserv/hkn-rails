57222b70d4a0cec8769f0b7350a48ea29e9febf2
Richard Xia <richardxia@hkn.eecs.berkeley.edu>
2010-05-23 17:04:56 -0700

    Initial import of project

---
+++ trunk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'e6ba732e0c7e33e7c7bd6bb3583c5d107dcf1fdcf6386d7bfe92251e774ec497c346c3cbfe2a4743486fc4a71c1c6c79cb3e64bb59a358cea3a04930a6a6b335'
