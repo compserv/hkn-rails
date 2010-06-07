9b21f4496b02a27474676111170a4e16c1d484e1
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-06-06 18:28:34 -0700

    Add validations to the Committeeship model.

--- app/models/committeeship.rb
+++ app/models/committeeship.rb
@@ -9,4 +9,10 @@ class Committeeship < ActiveRecord::Base
   #   updated_at : datetime 
   # =======================
 
+  Committees = %w(president vp rsec treas csec deprel act alumrel bridge compserv indrel serv studrel tutoring)	#This generates a constant which is an array of possible committees.
+  Semester = /(fa|sp)\d{2,2}/	#A regex which validates the semester
+  Positions = %w(officer cmember candidate)	#A list of possible positions
+  validates_inclusion_of :committee, :in => Committees, :message => "Committee not recognized."
+  validates_format_of :semester, :with => Semester, :message => "Not a valid semester."
+  validates_inclusion_of :title, :in => Positions, :message => "Not a valid title."  
 end
