a05d9f636186f5d085181c305d4ee01fad5eb7b1
Richard Xia <richardxia@arbiter.EECS.Berkeley.EDU>
2010-06-19 22:23:45 -0700

    Added committee preference spec

---
+++ spec/models/committee_preference_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe CommitteePreference, "when created with blank parameters" do
+  before(:each) do
+    @cp = CommitteePreference.create
+  end
+
+  it "should require a candidate" do
+    @cp.should_not be_valid
+    @cp.errors[:candidate].should include("can't be blank")
+  end
+
+  it "should require a committee (group)" do
+    @cp.should_not be_valid
+    @cp.errors[:group].should include("can't be blank")
+  end
+end
+
+describe CommitteePreference do
+  before(:each) do
+    @candidate = mock_model(Candidate)
+    @group = mock_model(Group)
+    @cp = CommitteePreference.create(:candidate => @candidate, :group => @group)
+  end
+
+  it "should be valid when supplying both a candidate and a group" do
+    @cp.should be_valid
+  end
+end
