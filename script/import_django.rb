6ff2a4e18580cf07b04e17252ff5ea68f7f14a38
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-06-20 23:59:04 -0700

    Some parsing of the auth data from django, into a ruby hash. Also maps officerships to people now.

--- script/import_django.rb
+++ script/import_django.rb
@@ -1,16 +1,26 @@
 require 'json'
 
+people = {}
+File.open('auth.json', 'r') do |f|
+  t = f.read
+  obj = JSON::load t
+  obj.each do |entry|
+    if entry["model"] == "auth.user"
+	  people[entry["pk"]]  = {:username => entry["fields"]["username"], :first => entry["fields"]["first_name"], :last => entry["fields"]["last_name"], :email => entry["fields"]["email"]}
+	end
+  end
+end
+p people
 File.open('info.json', 'r') do |f|
   t = f.read
   obj = JSON::load t
-  #p obj
-  p obj.class
   positions = []
   obj.each do |line|
 	if line["model"] == "info.position"
 	  positions[line["pk"]] = line["fields"]["short_name"]
 	end
     if line["model"] == "info.officership"
+	  p people[line["fields"]["person"]]
 	  p positions[line["fields"]["position"]], line["fields"]["semester"]
 	end
   end
