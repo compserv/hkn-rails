e943c7454a87b933ac6e28d8d8cd46903538d95e
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-01 08:25:19 -0700

    Another dummy commit so that I can test this hook again.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Group do
+  before(:each) do
+    @good_opts = {:name => "SomeGroup",
+      :description => "A random group"}
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
