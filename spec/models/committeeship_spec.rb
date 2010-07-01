3f2cefcc311010aeff6591c19043c738dc21b424
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-01 00:23:18 -0700

    Add some committeeship tests

--- spec/models/committeeship_spec.rb
+++ spec/models/committeeship_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Committeeship do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @good_opts = {:committee => 'compserv',
+      :semester => 'fa10',
+      :title => 'officer'
+    }
+    end
+  it "should accept valid parameters" do
+    committeeship = Committeeship.create(@good_opts)
+    committeeship.should be_valid
+  end
+  it "should require a valid semester" do
+    committeeship = Committeeship.create(@good_opts.merge(:semester=>'fa100'))
+    committeeship.should_not be_valid
+  end
 end
