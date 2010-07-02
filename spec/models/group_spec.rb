931166a6730599a6ac8a3534e67ac07af87d7101
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-01 20:13:03 -0700

    Group: added spec for name can't be blank

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -15,4 +15,8 @@ describe Group, "when created with blank parameters" do
   before(:each) do
     @group = Group.create
   end
+  it "should require a name" do
+    @group.should_not be_valid
+    @group.errors[:name].should include("can't be blank")
+  end
 end
