6060d1109001b8f9dc86c23f57724d1ecc1d3d69
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-03 16:08:09 -0700

    Gather a little more information, store committeeships with person hash

--- script/import_django.rb
+++ script/import_django.rb
@@ -1,28 +1,46 @@
-require 'json'
+require 'json/pure'
 
 people = {}
+positions = []
+
 File.open('auth.json', 'r') do |f|
   t = f.read
   obj = JSON::load t
   obj.each do |entry|
     if entry["model"] == "auth.user"
-	  people[entry["pk"]]  = {:username => entry["fields"]["username"], :first => entry["fields"]["first_name"], :last => entry["fields"]["last_name"], :email => entry["fields"]["email"]}
-	end
+      person  = {:username => entry["fields"]["username"],
+        :first_name => entry["fields"]["first_name"],
+        :last_name => entry["fields"]["last_name"],
+        :email => entry["fields"]["email"]}
+      passfields = entry["fields"]["password"].split '$'
+      person[:crypted_password] = passfields[2]
+      person[:password_salt] = passfields[1]
+      people[entry["pk"]] = person
+    end
   end
 end
-p people
 File.open('info.json', 'r') do |f|
   t = f.read
   obj = JSON::load t
-  positions = []
   obj.each do |line|
-	if line["model"] == "info.position"
-	  positions[line["pk"]] = line["fields"]["short_name"]
-	end
+    if line["model"] == "info.position"
+      positions[line["pk"]] = line["fields"]["short_name"]
+    end
     if line["model"] == "info.officership"
-	  p people[line["fields"]["person"]]
-	  p positions[line["fields"]["position"]], line["fields"]["semester"]
-	end
+      pid = line["fields"]["person"]
+      people[pid][:committeeships] ||= []
+      people[pid][:committeeships] << {
+        :committee => positions[line["fields"]["position"]],
+        :title => 'officer', :semester => line["fields"]["semester"] }
+    end
+    if line["model"] == "info.extendedinfo"
+      pid = line["pk"]
+      people[pid][:aim] = line["fields"]["aim_sn"]
+    end
   end
-  p positions
 end
+people.each do |k, v|
+  p k
+  p v
+end
+p positions
