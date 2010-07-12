b89bc40c1d392d63145ef66389551530145ef6c7
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-12 11:37:44 +0600

    Added courses_tutors join table

--- app/models/course.rb
+++ app/models/course.rb
@@ -12,6 +12,7 @@ class Course < ActiveRecord::Base
   #   updated_at    : datetime 
   # =======================
 
+  has_and_belongs_to_many :tutors
   validates :department, :presence => true
   validates :course_number, :presence => true
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100711094126) do
+ActiveRecord::Schema.define(:version => 20100712053418) do
 
   create_table "availabilities", :force => true do |t|
     t.integer  "tutor_id"
@@ -111,6 +111,11 @@ ActiveRecord::Schema.define(:version => 20100711094126) do
     t.integer  "tutor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "courses_tutors", :id => false, :force => true do |t|
+    t.integer "course_id"
+    t.integer "tutor_id"
   end
 
   create_table "coursesurveys", :force => true do |t|
