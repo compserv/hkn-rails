9cdca09a5d715bc72390155c86f8f0168732c994
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-12 21:59:24 +0600

    Slightly nicer formatting of hours on signup page

--- app/helpers/admin/tutor_admin_helper.rb
+++ app/helpers/admin/tutor_admin_helper.rb
@@ -1,11 +1,25 @@
 module Admin::TutorAdminHelper
   def formatslot(day, hour)
-    day[0..2] + hour
+    return day[0..2] + hour
   end
   
   def default_radio(user, day, hour, pref)
     slots_available = user.tutor.availability.slots.map { |x| x.to_s }
     available_now = slots_available.include? formatslot(day, hour)
-    'checked="checked"' if available_now ^ (pref == "unavailable")
+    return 'checked="checked"' if available_now ^ (pref == "unavailable")
+  end
+
+  def format_hour(hour)
+    if hour < 12
+      return hour.to_s + "AM"
+    elsif hour == 12
+      return hour.to_s + "PM"
+    else
+      return (hour-12).to_s + "PM"
+    end
+  end
+  
+  def format_hour_slot(hour)
+    return format_hour(hour) + "-" + format_hour(hour+1)
   end
 end
