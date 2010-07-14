28e55ae40ed4156086db10074675ca19f672e61f
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-14 11:46:01 +0600

    Added a filter that gets the current user in all pages. Woot.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_current_user
   layout 'application'
+  
+  private
+  
+  def get_current_user
+    if UserSession.find
+      @current_user = UserSession.find.person
+    end
+  end
 end
