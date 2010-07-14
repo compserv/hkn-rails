1ff47716ec68c07a1d15583631ac4ca762d83c9f
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-14 14:10:21 +0600

    Added a filter for authorizing users to access pages based on being logged in or being in a certain group.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :get_current_user
+  before_filter :get_current_user, :merge_messages
   layout 'application'
   
   private
@@ -8,6 +8,23 @@ class ApplicationController < ActionController::Base
   def get_current_user
     if UserSession.find
       @current_user = UserSession.find.person
+    end
+  end
+
+  def merge_messages
+    if flash[:notice]
+      @messages ||= []
+      @messages << flash[:notice]
+    end
+  end
+
+  def authorize(group=nil)
+    if @current_user.nil?
+      redirect_to :login, :notice => "Please log in to access this page."
+      return
+    end
+    unless group.nil? or @current_user.groups.map{|x| x.name}.include?(group)
+      redirect_to :root, :notice => "Insufficient privileges to access this page."
     end
   end
 end
