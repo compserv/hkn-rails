8fbf9ab64224ca4500bdf85c7ccd3400b17660b4
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-22 12:41:17 +0600

    Use the variables set in Property for the tutoring sidebar.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
     @show_searcharea = true
-    @tutoring_enabled = true
+    @tutoring_enabled = Property.tutoring_enabled
     @hours = Property.tutoring_start .. Property.tutoring_end
     @day = Time.now.strftime("%a")
+    @tutoring_message = Property.tutoring_message
   end
 
 end
