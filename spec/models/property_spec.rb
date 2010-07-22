d1bc1dc3998d126eb202a877663177f08827fb0e
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-22 11:45:26 +0600

    Change Property to match the new fields and use a magic helper to generate getters and setters for all valid properties.
    This breaks the property specs and I haven't been able to quite figure out how to fix some of them.

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -4,33 +4,15 @@ describe Property do
   before(:each) do
     Property.destroy_all
     @good_opts = {
-	:tutor_version => 1,
-	:semester => 'fa10'
+      :tutoring_enabled=>true,
+      :semester => 'fa10'
 	}
   end
-  it "should accept valid parameters" do
-	prop = Property.create(@good_opts)
-	prop.should be_valid
-  end
-  it "should not allow multiple properties" do
-	p1 = Property.create(@good_opts)
-	p1.should be_valid
-	p2 = Property.create(@good_opts)
-	p2.should_not be_valid
-	p2.errors[:base].should include("There can only be one property entry") 
-  end
   it "should require a valid semester" do
-    prop = Property.create(@good_opts.merge(:semester => 'fa100'))
-	prop.should_not be_valid
-	prop.errors[:semester].should include("Not a valid semester.")
-  end
-  it "should require a valid tutor version" do
-    prop = Property.create(@good_opts.merge(:tutor_version => 'over 9000'))
-	prop.should_not be_valid
-  end
-  it "should allow convenience getters and setters for tutor_version" do
-    Property.tutor_version = 4
-    Property.tutor_version.should == 4
+    prop = Property.get_or_create
+    prop.semester = "fa100"
+    prop.should_not be_valid
+    prop.errors[:semester].should include("Not a valid semester.")
   end
   it "should allow convenience getters and setters for semester" do
     Property.semester = 'sp10'
