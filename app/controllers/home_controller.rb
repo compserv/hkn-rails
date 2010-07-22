5b125ebea85acd83dad826b122a4c2fd648c7e79
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-22 11:53:46 +0600

    A rudimentary tutoring sidebar.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @show_searcharea = true
-    if UserSession.find
-      @current_user = UserSession.find.person
-    end
+    @tutoring_enabled = true
+    @hours = Property.tutoring_start .. Property.tutoring_end
+    @day = Time.now.strftime("%a")
   end
 
 end
