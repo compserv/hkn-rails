02dc9c8cda6ccd664a92fc0ac603861a3bc2b58c
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-23 22:18:17 +0600

    Handle weekends in the tutoring sidebar.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,13 @@ class HomeController < ApplicationController
     prop = Property.get_or_create
     @tutoring_enabled = prop.tutoring_enabled
     @hours = prop.tutoring_start .. prop.tutoring_end
-    @day = Time.now.strftime("%a")
+    if (1..5) === Time.now.wday
+      @day = Time.now.strftime("%a")
+      @tutor_title = "Today's tutoring schedule"
+    else
+      @day = "Mon"
+      @tutor_title = "Monday's tutoring schedule"
+    end
     @tutoring_message = prop.tutoring_message
   end
 
