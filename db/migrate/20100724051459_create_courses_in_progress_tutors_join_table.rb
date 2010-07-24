10e16a8f25340f772f295a7a1be410c754588178
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-24 11:22:14 +0600

    Add courses in progress to tutors.

---
+++ db/migrate/20100724051459_create_courses_in_progress_tutors_join_table.rb
@@ -0,0 +1,12 @@
+class CreateCoursesInProgressTutorsJoinTable < ActiveRecord::Migration
+  def self.up
+    create_table :courses_in_progress_tutors, :id => false do |t|
+      t.integer :course_id
+      t.integer :tutor_id
+    end
+  end
+
+  def self.down
+    drop_table :courses_in_progress_tutors
+  end
+end
