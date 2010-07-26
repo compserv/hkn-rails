1dc626c736f3538494c60f75d85e7cec8363f12a
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-07-26 22:59:46 +0600

    Only set tutoring variables if tutoring is enabled.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,17 @@ class HomeController < ApplicationController
     @show_searcharea = true
     prop = Property.get_or_create
     @tutoring_enabled = prop.tutoring_enabled
-    @hours = prop.tutoring_start .. prop.tutoring_end
-    if (1..5) === Time.now.wday
-      @day = Time.now.strftime("%a")
-      @tutor_title = "Today's tutoring schedule"
-    else
-      @day = "Mon"
-      @tutor_title = "Monday's tutoring schedule"
+    if @tutoring_enabled
+      @hours = prop.tutoring_start .. prop.tutoring_end
+      if (1..5) === Time.now.wday
+        @day = Time.now.strftime("%a")
+        @tutor_title = "Today's tutoring schedule"
+      else
+        @day = "Mon"
+        @tutor_title = "Monday's tutoring schedule"
+      end
+      @tutoring_message = prop.tutoring_message
     end
-    @tutoring_message = prop.tutoring_message
   end
 
 end
