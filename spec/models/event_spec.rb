21d6ec22f68d25db093b17149d66ea63b4fd2854
Richard Xia <richardxia@arbiter.EECS.Berkeley.EDU>
2010-07-28 23:03:26 -0700

    Fixed event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -15,10 +15,14 @@ describe Event do
   before(:each) do
     @good_opts = {:name => "Fun Event", 
       :start_time => DateTime.parse("2010-05-26 18:00:00 UTC"),
-      :end_time => DateTime.parse("2010-05-26 20:00:00 UTC")}
+      :end_time => DateTime.parse("2010-05-26 20:00:00 UTC"),
+      :location => "The Woz",
+      :description => "Very fun event"
+    }
     @bad_opts = {:name => "Fun Event", 
       :start_time => DateTime.parse("2010-05-26 18:00:00 UTC"),
-      :end_time => DateTime.parse("2010-05-26 16:00:00 UTC")}
+      :end_time => DateTime.parse("2010-05-26 16:00:00 UTC")
+    }
   end
 
   it "should accept valid parameters" do
@@ -26,9 +30,21 @@ describe Event do
     event.should be_valid
   end
 
+  it "should require a location" do
+    event = Event.create(@bad_opts)
+    event.should_not be_valid
+    event.errors[:location].should include("can't be blank")
+  end
+
+  it "should require a description" do
+    event = Event.create(@bad_opts)
+    event.should_not be_valid
+    event.errors[:description].should include("can't be blank")
+  end
+
   it "should require the start time to be earlier than the end time" do
     event = Event.create(@bad_opts)
     event.should_not be_valid
-    event.errors[:base].should include("Start time must be less than end time")
+    event.errors[:end_time].should include("must be after start time")
   end
 end
