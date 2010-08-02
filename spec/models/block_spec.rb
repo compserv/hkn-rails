687f40358d1b3ed3efe5b6be614af679f8641cc4
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-08-01 23:03:50 -0700

    Fix block spec to match recent changes to the block model.

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -5,19 +5,16 @@ describe Block, "when created with blank parameters" do
     @block = Block.create
   end
 
-  it "should not require any fields to be valid" do
-    @block.should be_valid
-  end
-
-  it "should" do
-    
+  it "should not be valid" do
+    @block.should_not be_valid
   end
 end
 
 describe Block, "when created with date parameters" do
   before(:each) do
     @good_opts = {:start_time => DateTime.parse("2010-07-05 16:00:00 UTC"),
-      :end_time => DateTime.parse("2010-07-05 18:00:00 UTC")}
+      :end_time => DateTime.parse("2010-07-05 18:00:00 UTC"),
+	  :event => Event.create}
     @bad_opts = {:start_time => DateTime.parse("2010-07-05 18:00:00 UTC"),
       :end_time => DateTime.parse("2010-07-05 16:00:00 UTC")}
   end
@@ -30,6 +27,6 @@ describe Block, "when created with date parameters" do
   it "should not accept an end time before a start time" do
     block = Block.create(@bad_opts)
     block.should_not be_valid
-    block.errors[:base].should include("Start time must be less than end time")
+    block.errors[:end_time].should include("must be after start time")
   end
 end
