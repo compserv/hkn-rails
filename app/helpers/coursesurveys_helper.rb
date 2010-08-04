bd37946801c7a2c7197b9688252d2a1e7109a749
Richard Xia <richardxia@arbiter.EECS.Berkeley.EDU>
2010-08-04 03:52:45 -0700

    Made a few cosmetic changes

--- app/helpers/coursesurveys_helper.rb
+++ app/helpers/coursesurveys_helper.rb
@@ -3,11 +3,13 @@ module CoursesurveysHelper
   def rating_bar(rating, url=nil, inverted=nil)
     width = (rating*100).to_int
     color = (rating > 0.75) ?  "green" : (rating > 0.5) ? "orange" : "red"
-    
+
     # I appologize for forcing a span to be displayed as a block, I can't 
     # think of a better way of making it clickable with a link
+    # Currently there is a bug in Chrome for the inverted style where there
+    # is a one-pixel gap on the right
     outer_html_options = { :class => "ratingbar" }
-    inner_html_options = { :class => "subbar", :style => "width: #{width}%; background-color: #{color};" }
+    inner_html_options = { :class => "subbar", :style => "width: #{width}%; background-color: #{color};#{ " margin-left: #{100-width}%;" if inverted }" }
     if url.nil?
       content_tag(:span, outer_html_options) do
         content_tag("span", "", inner_html_options)
