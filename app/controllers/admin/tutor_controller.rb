c0667f85937429d5dfa309c9d7139a2dc44b82ba
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-08-15 15:55:55 -0700

    Officers should be able to sign up for courses too.

--- app/controllers/admin/tutor_controller.rb
+++ app/controllers/admin/tutor_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TutorController < Admin::AdminController
-  before_filter :authorize_tutoring, :except=>[:signup_slots]
+  before_filter :authorize_tutoring, :except=>[:signup_slots, :signup_courses]
   
   def signup_slots
     tutor = @current_user.get_tutor
