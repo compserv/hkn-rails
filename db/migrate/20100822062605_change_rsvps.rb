d877d96ccdb6639e27cc652a3a6c04b284f2a0b8
Richard Xia <richardxia@arbiter.EECS.Berkeley.EDU>
2010-08-23 12:41:51 -0700

    Fixed error with change_rsvps migration

--- db/migrate/20100822062605_change_rsvps.rb
+++ db/migrate/20100822062605_change_rsvps.rb
@@ -1,11 +1,11 @@
 class ChangeRsvps < ActiveRecord::Migration
   def self.up
-    puts "Warning: Dropping all rows from rsvps"
-    Rsvp.delete_all
     create_table :blocks_rsvps, :id => false do |t|
       t.integer :block_id
       t.integer :rsvp_id
     end
+    puts "Warning: Dropping all rows from rsvps, since there is a major change to RSVPs."
+    Rsvp.delete_all
     remove_column :rsvps, :block_id
   end
 
