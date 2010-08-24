53f92b007e37ac768bdc5d5e8fb70461ddfbac1f
Richard Xia <richardxia@arbiter.EECS.Berkeley.EDU>
2010-08-23 18:14:59 -0700

    Renamed DjangoSha1.rb to django_sha1.rb to match naming convention. Fixed annoying namespacing bug by requiring django_sha1 in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module HknRails
   class Application < Rails::Application
+    require 'django_sha1'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
