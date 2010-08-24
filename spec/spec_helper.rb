4bb02f84fa785455f12b2306645073e62848b5a6
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2010-08-23 19:23:37 -0700

    Added helpers for authlogic-related spec stuff, from http://iain.nl/2008/11/authlogic-is-awesome/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,3 +25,19 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
+def current_user(stubs = {})
+  @current_user ||= mock_model(User, stubs)
+end
+
+def user_session(stubs = {}, user_stubs = {})
+  @current_user ||= mock_model(UserSession, {:user => current_user(user_stubs)}.merge(stubs))
+end
+
+def login(session_stubs = {}, user_stubs = {})
+  UserSession.stub!(:find).and_return(user_session(session_stubs, user_stubs))
+end
+
+def logout
+  @user_session = nil
+end
