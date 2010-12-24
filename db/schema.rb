82b1d5cdb97301d3585af3089dedca1601569d52
Richard Xia <richardxia@ghost.(none)>
2010-12-23 19:33:07 -0800

    Added authlogic access tokens to company model, added company session model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101222061717) do
+ActiveRecord::Schema.define(:version => 20101224020437) do
 
   create_table "announcements", :force => true do |t|
     t.string   "title"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(:version => 20101222061717) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "comments"
+    t.string   "persistence_token",   :default => "", :null => false
+    t.string   "single_access_token", :default => "", :null => false
   end
 
   create_table "contacts", :force => true do |t|
