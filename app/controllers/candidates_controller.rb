2aac9845963a1f2bb599cddf676cef36a751c5c7
Amber Feng <amber@ghost.(none)>
2010-12-25 13:16:42 -0800

    Skeleton for checking candidate requirements

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,21 @@
 class CandidatesController < ApplicationController
+  before_filter :is_candidate?
+  
+  def is_candidate?
+    if @current_user
+      if !@current_user.in_group?("candidates")
+        flash[:notice] = "You're not a candidate anymore, so this information may not apply to you"
+      end
+      return true
+    else
+      flash[:notice] = "You must be logged in to view that page."
+      redirect_to "/"
+    end
+  end
+
   def portal
+    @announcements = Announcement.order("created_at desc").limit(10)
+    @requirements = @current_user.candidate.requirements_status
   end
 
   def application
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -13,5 +13,27 @@ class Candidate < ActiveRecord::Base
   has_many :committee_preferences
 
   validates :person, :presence => true
+  
+  def event_requirements
+    req = Hash.new
+    req["Mandatory for Candidates"] = -1
+    req["Fun"] = 3
+    req["Big Fun"] = 1
+    req["Community Service"] = 2
+  end
 
+  def requirements_status
+    rsvps = self.person.rsvps
+    done = Hash.new
+    for rsvp in rsvps #Record finished requirements
+      event = rsvp.event
+      done[event.event_type] = done[event.event_type] + 1
+    end
+    status = Hash.new #Record status
+    reqs = event_requirements
+    reqs.each do |key, value|
+      
+    end
+    return {:status => status, :rsvps => rsvps}
+  end
 end
