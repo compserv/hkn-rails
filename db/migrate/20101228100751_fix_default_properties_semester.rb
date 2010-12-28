52e8aaf00de14a45cef561217ac9e99ed58ffd06
Conor Hughes <conorh@conorh.net>
2010-12-28 02:11:59 -0800

    Fix default for the Properties class' semester attr

---
+++ db/migrate/20101228100751_fix_default_properties_semester.rb
@@ -0,0 +1,9 @@
+class FixDefaultPropertiesSemester < ActiveRecord::Migration
+  def self.up
+    change_column_default("properties","semester","20103")
+  end
+
+  def self.down
+    change_column_default("properties","semester","fa10")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101228035445) do
+ActiveRecord::Schema.define(:version => 20101228100751) do
 
   create_table "announcements", :force => true do |t|
     t.string   "title"
@@ -297,7 +297,7 @@ ActiveRecord::Schema.define(:version => 20101228035445) do
   end
 
   create_table "properties", :force => true do |t|
-    t.string   "semester",         :default => "fa10"
+    t.string   "semester",         :default => "20103"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "tutoring_enabled", :default => false
