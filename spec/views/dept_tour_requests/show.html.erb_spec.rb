da21b8339d937e4b6f3c0a1e0d0cc3b40e669126
Conor Hughes <conorh@conorh.net>
2011-01-01 19:43:20 -0800

    fix a failing rspec test

--- spec/views/dept_tour_requests/show.html.erb_spec.rb
+++ spec/views/dept_tour_requests/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "dept_tour_requests/show.html.erb" do
   before(:each) do
     @dept_tour_request = assign(:dept_tour_request, stub_model(DeptTourRequest,:date=>Time.now))
-    @current_user = assign(:current_user, mock_model(Person,:email=>"jdoe@example.com"))
+    @current_user = assign(:current_user, mock_model(Person,:email=>"jdoe@example.com",:first_name=>"John"))
   end
   
   it "renders attributes in <p>" do
