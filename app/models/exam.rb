4b7c413433a1c5aa1913a6502d868d090c569890
Alex Degtiar <adegtiar@ghost.(none)>
2011-01-04 13:09:18 -0800

    Cleaned up exam model and fixed inclusion/presence bug.

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -20,27 +20,23 @@ class Exam < ActiveRecord::Base
   validates :course, :presence => true
   validates :filename, :presence => true
   validates :exam_type, :presence => true
-  validates :is_solution, :presence => true
+  validates_inclusion_of :is_solution, :in => [true, false]
+
+  @@TYPE_ABBRS = { 0 => 'q', 1 => 'mt', 2 => 'f' }
+  @@TYPE_NAMES = { 0 => 'quiz', 1 => 'midterm', 2 => 'final'}
+  @@TYPE_NUMS = {'q' => 0, 'mt' => 1, 'f' => 2 }
 
   #Returns the name of the exam type
   def type_name
-    if exam_type == 0 then
-      "quiz"
-    elsif exam_type == 1 then
-      "midterm"
-    elsif exam_type == 2 then
-      "final"
-    end
+    @@TYPE_NAMES[exam_type]
   end
 
   #Returns the abbreviation of the exam type
   def type_abbr
-    if exam_type == 0 then
-      "q"
-    elsif exam_type == 1 then
-      "mt"
-    elsif exam_type == 2 then
-      "f"
-    end
+    @@TYPE_ABBRS[exam_type]
+  end
+
+  def Exam.typeFromAbbr(abbr)
+    @@TYPE_NUMS[abbr]
   end
 end
