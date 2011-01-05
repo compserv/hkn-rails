0a417ec38b49fed01b01f02e7f45e55e35ee2e73
Richard Xia <richardxia@hkn.eecs.berkeley.edu>
2011-01-04 18:43:05 -0800

    Removed Gemfile.lock and added gitignore rule. Updated schema.rb

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,3 +1,16 @@
 class Alumni < ActiveRecord::Base
+
+  # === List of columns ===
+  #   id            : integer 
+  #   grad_semester : string 
+  #   grad_school   : string 
+  #   job_title     : string 
+  #   company       : string 
+  #   salary        : integer 
+  #   person_id     : integer 
+  #   created_at    : datetime 
+  #   updated_at    : datetime 
+  # =======================
+
   belongs_to :person
 end
