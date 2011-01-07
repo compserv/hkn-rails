eb896827cca38d3de30d4e2ec05a719375d7155a
Amber Feng <amber@ghost.(none)>
2011-01-06 16:28:51 -0800

    part one of redoing the design..unfinished

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -12,27 +12,31 @@
   richardxia: ported this to Prototype
  */
 $(document).ready(function() {
+  $("#logo").click(function(){
+    window.location = "/";
+  });
 	current = location.pathname.replace('/','');
   if (current.indexOf('/') != -1)
     current = current.substring(0,current.indexOf('/'));
 	id = current;	
 	if (id.length != 0 && id != "coursesurveys" && $("#" + id).length != 0) {
-		$("#" + id).addClass("selected");
+		$("#" + id).addClass("current");
 		$("#" + current + "_submenu").show();
 	}
 	else {
-		$(".navigation_toplevel_item:first-child").first().addClass("selected");	
+		$($(".navigation_toplevel_item")[0]).addClass("current");	
 		$(".submenu").first().show();
 	}
 	$(".navigation_toplevel_item").click(
       function () {
-        $(".navigation_toplevel_item").removeClass("selected");
-        $(this).addClass("selected");
+        $(".navigation_toplevel_item").removeClass("current");
+        $(this).addClass("current");
         $(this).blur();
         $(".submenu").hide();
         id = $(this).attr("id") + "_submenu";
-        //$(id).fade({ duration: 0.2 });		
+        //$(id).fadeIn({ duration: 0.2 });		
 		    $("#" + id).show();
       });
+  
 });
 
