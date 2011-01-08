ffcf7c1d29d5d70f917bff2f66eabd0c675acaf7
Amber Feng <amber@ghost.(none)>
2011-01-07 23:56:00 -0800

    user bar

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
+    $("#user").click(function(){
+      $("#userbar").toggle("fast");
+    });
     var frame;
-
-    username="unknown-user";
+    username="guest";
     $("#chat").click(function() {
         if (!frame) {
             frame = $('<iframe name="chat" id="chatbox" src="http://ghost.eecs.berkeley.edu:9999?nick=' + username + '&channels=hkn-chat&prompt=0"></iframe>');
             $("#chatwindow").append(frame);
-            
-            var cssLink = document.createElement("link") 
-            cssLink.href = "chat.css"; 
-            cssLink .rel = "stylesheet"; 
-            cssLink .type = "text/css"; 
-            frames['chat'].document.body.appendChild(cssLink);
         }
         $("#chatwindow").toggle("fast");
         return false;
