6946d5d0414977a69614e3b6d202a026431cb03c
Richard Xia <richardxia@hkn.eecs.berkeley.edu>
2011-01-11 14:30:14 -0800

    Removed null constraint for course's name property. You can blame the school for allowing courses without names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110111204703) do
+ActiveRecord::Schema.define(:version => 20110111222443) do
 
   create_table "alumnis", :force => true do |t|
     t.string   "grad_semester"
@@ -124,7 +124,7 @@ ActiveRecord::Schema.define(:version => 20110111204703) do
   create_table "courses", :force => true do |t|
     t.string   "suffix",        :default => ""
     t.string   "prefix",        :default => ""
-    t.string   "name",                          :null => false
+    t.string   "name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
