b127c6abd223d97555486d07b695d8b2e4e96c0a
Amber Feng <amber@ghost.(none)>
2011-01-12 12:15:18 -0800

    candidate req finished logic

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -19,6 +19,16 @@ class CandidatesController < ApplicationController
     @status = requirements[:status]
     @rsvps = requirements[:rsvps]
     @events = Event.order("start_time asc").limit(5)
+    
+    @done = Hash.new(false) #events, challenges, forms, resume, quiz, course_surveys
+    @done["events"] = !@status.has_value?(false)
+    @done["challenges"] = @current_user.candidate.challenges.select {|c| c.status }.length >= 5
+    @done["resume"] = @current_user.resumes.length >= 0
+    @done["quiz"] = @current_user.candidate.quiz_responses.length >= 0
+    @done["forms"] = @done["resume"] and @done["quiz"]
+    @done["course_surveys"] = false
+    
+    
   end
   
   def find_officers #FIXME: what's a more efficient way to do this?
