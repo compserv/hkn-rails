80c7f564d955e01f714553c97a1594664818dba9
Jonathan Ko <jonathanko@hkn.eecs.berkeley.edu>
2011-01-13 00:05:43 -0800

    Added initializer code to dynamically disable sunspot, so you can actually create and destroy ActiveRecord::Bases without errors when solr isn't running.

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,1 +1,13 @@
 Sunspot.config.pagination.default_per_page = 50
+
+# Fake out sunspot if the server isn't running.
+# Otherwise, model save/destroy will throw an error.
+begin
+  File.file?(pidfile=Sunspot::Server.new.pid_path)
+  pid=IO.read(pidfile).to_i
+  Process.kill(0,pid) # check if running (doesn't actually kill)
+  # must be running...
+rescue Exception # not running
+  # puts "Note: Sunspot server isn't running. Search may be crippled, and your indices may be incomplete if you create or destroy any ActiveRecord::Bases." unless RAILS_ENV.eql?('test')
+  Sunspot.session = Sunspot::Rails::StubSessionProxy.new(Sunspot.session)
+end
