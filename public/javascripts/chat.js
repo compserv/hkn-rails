fc6cd25ed2996e464d719aa9524473b4ca159c49
Amber Feng <amber@ghost.(none)>
2011-01-13 00:00:36 -0800

    use logged in username as irc username

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
       $("#userbar").toggle("fast");
     });
     var frame;
-    username="guest";
+    username= $("#username").html();
     $("#chat").click(function() {
         if (!frame) {
             frame = $('<iframe name="chat" id="chatbox" src="http://ghost.eecs.berkeley.edu:9999?nick=' + username + '&channels=hkn&prompt=0"></iframe>');
