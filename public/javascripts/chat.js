05e3fe5eeff9d3ba203640e65be6f30ece86e391
Amber Feng <amber@ghost.(none)>
2011-01-12 23:53:44 -0800

    changed irc to #hkn

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     username="guest";
     $("#chat").click(function() {
         if (!frame) {
-            frame = $('<iframe name="chat" id="chatbox" src="http://ghost.eecs.berkeley.edu:9999?nick=' + username + '&channels=hkn-chat&prompt=0"></iframe>');
+            frame = $('<iframe name="chat" id="chatbox" src="http://ghost.eecs.berkeley.edu:9999?nick=' + username + '&channels=hkn&prompt=0"></iframe>');
             $("#chatwindow").append(frame);
         }
         $("#chatwindow").toggle("fast");
