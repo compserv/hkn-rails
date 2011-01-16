f4f84c8971f0a9fcfa35788c47a45db213c2bc85
Tony Dear <tonydear@hkn.eecs.berkeley.edu>
2011-01-16 12:51:48 -0800

    Inserted the popup div in the slot view. Updated course_preference.rb for later use (need to figure out a way to do the course highlighting stuff..)

--- app/models/course_preference.rb
+++ app/models/course_preference.rb
@@ -9,6 +9,7 @@ class CoursePreference < ActiveRecord::Base
   #   updated_at : datetime 
   # =======================
 
+  #Level 0 = current, Level 1 = completed, Level 2 = preferred
 
   belongs_to :course
   belongs_to :tutor
@@ -18,5 +19,20 @@ class CoursePreference < ActiveRecord::Base
   validates :level, :presence => true
   validates_numericality_of :level, :only_integer => true, :message => "can only be whole number."
   validates_inclusion_of :level, :in => 0..2, :message => "invalid value." 
+
+  #For scheduler class view
+  def CoursePreference.all_courses
+    ret = Hash.new()
+    for cpref in CoursePreference.all
+      course = cpref.course
+      if ret[course.dept_abbr].nil?
+        ret[course.dept_abbr] = []
+      end
+      if not ret[course.dept_abbr].include?(course.full_course_number)
+        ret[course.dept_abbr] << course.full_course_number
+      end
+    end
+    return ret
+  end
 
 end
