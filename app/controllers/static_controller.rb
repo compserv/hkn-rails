6e8de6aa3422c9424fca0d521b25a3d6f7b98b53
Amber Feng <amber@ghost.(none)>
2011-01-27 22:04:40 -0800

    static page caching

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,24 @@
 class StaticController < ApplicationController
-  def indrel
-    @hide_topbar = true
+  caches_page :coursesurveys_how_to, :coursesurveys_info_profs, :coursesurveys_ferpa, :contact, :comingsoon, :yearbook, :slideshow
+  
+	def coursesurveys_how_to
+	end
+	
+	def coursesurveys_info_profs
+	end
+	
+	def coursesurveys_ferpa
+  end
+  
+  def contact
+  end
+  
+  def comingsoon
+  end
+  
+  def yearbook
+  end
+  
+  def slideshow
   end
 end
