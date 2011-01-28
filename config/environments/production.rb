b21cfe010851ffe950e1302161d8258ab0c8918d
Jonathan Ko <jonathanko@hkn.eecs.berkeley.edu>
2011-01-28 01:48:42 -0800

    Enabling production static asset server, because Passenger doesn't play nicely with Apache's mod_rewrite. Cached assets should now be served from /public/cache on production site. I have no idea about performance implications.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ HknRails::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true  # apache can't intercept cache redirects with phusion passenger...
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
