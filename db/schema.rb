82f297a7f4814bb3ce72a5c059097e9dbca98722
Jonathan Ko <jonathanko@hkn.eecs.berkeley.edu>
2011-03-31 13:05:24 -0700

    Committing schema from latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110328201821) do
+ActiveRecord::Schema.define(:version => 20110331200008) do
 
   create_table "alumnis", :force => true do |t|
     t.string   "grad_semester"
@@ -180,7 +180,7 @@ ActiveRecord::Schema.define(:version => 20110328201821) do
     t.datetime "submitted"
     t.string   "contact"
     t.string   "phone"
-    t.string   "comments"
+    t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "responded",  :default => false
