85dac8ae79db2356266e77ce6b5682b8ff464756
Alex Degtiar <adegtiar@ghost.(none)>
2011-04-10 05:33:07 -0700

    Fixed instructor selection bug.

--- script/import_klasses/import_klasses.rb
+++ script/import_klasses/import_klasses.rb
@@ -15,7 +15,7 @@ require File.expand_path('../../../config/environment', __FILE__)
 def get_instructor(first_initials, last_name)
     # TODO check by first initial
     instructors = Instructor.find_all_by_last_name(last_name)
-    instructors.reject do |instructor|
+    instructors.reject! do |instructor|
       instructor.first_name.empty? or instructor.first_name[0] != first_initials[0]
     end
     if !instructors.empty?
