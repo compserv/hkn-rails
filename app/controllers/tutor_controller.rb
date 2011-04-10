adfc7147e969f79b04582d588bd800f8de6604d1
Tony Dear <tonydear@hkn.eecs.berkeley.edu>
2011-04-09 23:30:37 -0700

    Can now add review sessions and exam dates via New Event. Academic calendar should only show those two types; events calendar should show all but those two.

--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -11,4 +11,26 @@ class TutorController < ApplicationController
     @rows = ["Hours"] + @hours.to_a
   end
 
+  def calendar
+    month = (params[:month] || Time.now.month).to_i
+    year = (params[:year] || Time.now.year).to_i
+    # TODO: Fix this, I think we have timezone issues
+    @start_date = Time.local(year, month).beginning_of_month
+    @end_date = Time.local(year, month).end_of_month
+    @events = Event.with_permission(@current_user).find(:all, :conditions => { :start_time => @start_date..@end_date }, :order => :start_time)
+    @events = @events.select {|e| EventType.find(:all, :conditions => ["name IN (?)", ["Exam", "Review Session"]]).include?(e.event_type)}
+    # Really convoluted way of getting the first Sunday of the calendar, 
+    # which usually lies in the previous month
+    @calendar_start_date = (@start_date.wday == 0) ? @start_date : @start_date.next_week.ago(8.days)
+    # Ditto for last Saturday
+    @calendar_end_date = (@end_date == 0) ? @end_date.since(6.days) : @end_date.next_week.ago(2.days)
+
+    respond_to do |format|
+      format.html
+      format.js {
+        render :partial => 'calendar'
+      }
+    end
+  end
+
 end
