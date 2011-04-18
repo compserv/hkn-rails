a7f9ce981cc2c024b49733a55536cbc87c3a86e9
Richard Xia <richardxia@hkn.eecs.berkeley.edu>
2011-04-18 15:11:07 -0700

    Added list of unconfirmed challenges to super page

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,49 +3,50 @@ class Admin::AdminController < ApplicationController
   before_filter :authorize_comms, :except=>[:signup_slots, :signup_courses, :update_slots, :add_course, :find_courses]
 
   def super_page
-	@candidates = Candidate.all
-	@candidates.map! { |cand|
-	    calculate_status(cand)    
-	}
+    @candidates = Candidate.all
+    @candidates.map! { |cand|
+      calculate_status(cand)    
+    }
 
-	puts @candidates
+    puts @candidates
   end
-  
+
   def calculate_status(cand)
-	puts cand
-	done = Hash.new(false)
+    puts cand
+    done = Hash.new(false)
 
-	if cand.person_id == nil
-		done["events"] = []
-		return done
-	end
-        done["candidate"] = cand.person.full_name
-	done["events"] = cand.requirements_count 
+    if cand.person_id == nil
+      done["events"] = []
+      return done
+    end
+    done["candidate"] = cand.person.full_name
+    done["events"] = cand.requirements_count 
 
-	puts "REQ COUNT"
-	puts cand.requirements_count
-	puts "=="
-	done["challenges"] = cand.challenges.select {|c| c.status }.length
-	done["resume"] = cand.person.resumes.length
-	done["quiz"] = cand.quiz_responses.length
-	done["course_surveys"] = false
-	
-	puts done.to_s
-	return done
+    puts "REQ COUNT"
+    puts cand.requirements_count
+    puts "=="
+    done["challenges"] = cand.challenges.select {|c| c.status }.length
+    done["unconfirmed_challenges"] = cand.challenges.where :status => nil
+    done["resume"] = cand.person.resumes.length
+    done["quiz"] = cand.quiz_responses.length
+    done["course_surveys"] = false
+
+    puts done.to_s
+    return done
   end 
 
   def candidate_announcements
     @announcements = Announcement.order("created_at desc")
     render "admin/candidate_announcements"
   end
-  
+
   def create_announcement
     a = Announcement.new
     a.title = params[:title]
     a.body = params[:body]
     a.person = @current_user
     saved = a.save
-    
+
     if saved
       flash[:notice] = "Announcement created."
       #render :json => [true]
@@ -55,18 +56,18 @@ class Admin::AdminController < ApplicationController
     end
     redirect_to :back
   end
-  
+
   def edit_announcement
     @announcement = Announcement.find(params[:id])
     render "admin/edit_announcement"
   end
-  
+
   def update_announcement
     a = Announcement.find(params[:id])
     a.title = params[:title]
     a.body = params[:body]
     saved = a.save
-    
+
     if saved
       flash[:notice] = "Announcement updated."
       redirect_to "/admin/general/candidate_announcements"
@@ -76,17 +77,17 @@ class Admin::AdminController < ApplicationController
       redirect_to :back
       #render :json => [false, "Error."]
     end
-    
+
   end
-  
+
   def delete_announcement
     a = Announcement.find(params[:id])
     a.destroy
-    
+
     flash[:notice] = "Announcement has been deleted."
     redirect_to :back
   end
-  
+
   def confirm_challenges
     challenges = Challenge.find(:all, :conditions => {:officer_id => @current_user.id})
     @acc_challenges = challenges.select {|c| c.status }
@@ -94,12 +95,12 @@ class Admin::AdminController < ApplicationController
     @rejected_challenges = challenges.select {|c| c.status == false}
     render "admin/confirm_challenges"
   end
-  
+
   def confirm_challenge
     challenge = Challenge.find(params[:id])
     challenge.status = true
     challenge.save
-    
+
     flash[:notice] = "Challenge confirmed."
     redirect_to :back
   end
@@ -107,7 +108,7 @@ class Admin::AdminController < ApplicationController
     challenge = Challenge.find(params[:id])
     challenge.status = false
     challenge.save
-    
+
     flash[:notice] = "Challenge rejected."
     redirect_to :back
   end
