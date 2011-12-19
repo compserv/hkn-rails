5c1ed6ca5e8da61bb0808d01d8a68c2323b3e666
Richard Xia <richardxia@hkn.eecs.berkeley.edu>
2011-12-19 01:43:01 -0800

    Modified migrations for Slot and Availabilities
    Both call destroy_all on their respective models. I hope that this isn't
    going to be a problem because no one is using the ruby1.9 branch yet.

--- db/migrate/20111216103929_rewrite_slots.rb
+++ db/migrate/20111216103929_rewrite_slots.rb
@@ -1,6 +1,8 @@
 class RewriteSlots < ActiveRecord::Migration
   def change
-    say "WARNING: YOU MUST DESTROY ALL CURRENT (OFFICE HOUR) Slots TO RUN THIS MIGRATION!"
+    say "WARNING: DESTROYING ALL CURRENT (OFFICE HOUR) Slots TO RUN THIS MIGRATION!"
+    say "RERUN rake db:seed to fill out Slots"
+    Slot.destroy_all
     add_column :slots, :hour, :integer, :null => false
     add_column :slots, :wday, :integer, :null => false
     remove_column :slots, :time
