456c6dde4c7767ce8eb6bbc8a3d9d3977b9559f4
Tiffany Wang <twang@ghost.(none)>
2012-05-19 03:12:57 -0700

    alumni contact info feature (forgot the db migration)

---
+++ db/migrate/20120515094922_add_contact_info_to_alumni.rb
@@ -0,0 +1,15 @@
+class AddContactInfoToAlumni < ActiveRecord::Migration
+  def up
+    add_column :alumnis, :perm_email, :string
+    add_column :alumnis, :location, :string
+    add_column :alumnis, :suggestions, :text
+    add_column :alumnis, :mailing_list, :boolean
+  end
+  
+  def down
+    remove_column :alumnis, :perm_email
+    remove_column :alumnis, :location
+    remove_column :alumnis, :suggestions
+    remove_column :alumnis, :mailing_list
+  end
+end
