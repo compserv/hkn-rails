eb6feee33ea09bd80a8b1c7c9cf70387230dff4f
Donny Reynolds <donnywith@gmail.com>
2013-12-01 02:40:55 -0800

    oh boy finally got all the pesky error checking into place

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -12,8 +12,30 @@ class ProductController < ApplicationController
   end
 
   def create
-    @product = Sellable.new(params[:product])
+
+    if image_info = params[:product][:image_info]
+      File.open(img_location(params[:pid]),"wb") do |f|
+        f.write(image_info.read)
+      end
+      params[:product][:image] = create_image_field params[:pid]
+    else
+      flash[:notice] = "Please attach picture"
+      render 'new'
+      return
+    end
+
+    # Now we can create a record
+    @product = Sellable.new(params[:product].except(:image_info))
+
+    unless @product.valid?
+      flash[:notice] = "Please fill out all the fields"
+      render 'new'
+      return
+    end
+
+
     @product.save
+    flash[:notice] = "Saved"
     redirect_to show_path(@product)
   end
 
@@ -24,23 +46,15 @@ class ProductController < ApplicationController
   def update
     @product = Sellable.find(params[:pid])
 
-    if @product.update_attributes(params[:product].except(:image))
-    else
+    unless @product.update_attributes(params[:product].except(:image_info))
       render 'edit'
       return
     end
 
-    img_path = "public/pictures/product_images/#{@product.id}.png"
-    unless img = params[:product][:image]
-      flash[:notice] = "Please attach picture"
-      render 'edit'
-      return
+    if image_info = params[:product][:image_info]
+      load_image(@product, image_info)
     end
-    File.open(img_path,"wb") do |f|
-      f.write(img.read)
-    end
-    @product.image = "/pictures/product_images/#{@product.id}.png"
-    
+
     @product.save
     flash[:notice] = "Saved"
     redirect_to show_path(@product) 
@@ -51,6 +65,22 @@ class ProductController < ApplicationController
     @product = Sellable.find(params[:pid])
     @product.destroy
     redirect_to store_path
+  end
+
+
+  private
+  def load_image(product, image_info)
+    File.open(product.image_location(),"wb") do |f|
+      f.write(image_info.read)
+    end
+  end
+
+  def img_location(pid)
+    "public/pictures/product_images/#{pid}.png"
+  end
+
+  def create_image_field(pid)
+    "/pictures/product_images/#{pid}.png"
   end
 
 end
--- app/models/sellable.rb
+++ app/models/sellable.rb
@@ -11,8 +11,20 @@ class Sellable < ActiveRecord::Base
   #   image       : string 
   # =======================
 
-	validates :name,  :presence => true
-  	validates :price,   :presence => true
-  	validates :category, :presence=> true
+	validates	:name,		presence: true
+  	validates 	:price,		presence: true
+  	validates 	:category,	presence: true
+  	validates 	:image, 	presence: true
+
+  	# Returns the raw location for saving purposes
+  	def image_location()
+  		return "public/pictures/product_images/#{id}.png"
+  	end
+
+  	# Save the image field, so image_tag(product.image) can be used
+  	def initialize_image_field()
+  		image = "/pictures/product_images/#{id}.png"
+  	end
+
 
 end
