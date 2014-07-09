1ae5b9d2326fa8f70c65cbe7b8691f5e60d8dd5b
Jason Khoe <jasonk47@gmail.com>
2014-07-09 00:39:44 -0700

    Increased coverage of challenge spec to 100% PT=74575756

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -15,6 +15,40 @@ describe Challenge do
       end
     end
 
+    it "raises ArgumentError with a bad argument" do
+      bogus_status = "wat"
+
+      challenge = stub_model(Challenge)
+      allow(challenge).to receive(:status).and_return(bogus_status)
+
+      expect { challenge.get_status_string }.to raise_error
+    end
+
   end
 
+  # TODO Make spec implementation independent
+  #
+  # The reason this spec is implementation dependent is due to the stubbed
+  # method chains. The only way I could think of around it is to actually
+  # create model instances, which would slow down the test.
+  describe "is_current_challenge?" do
+    before(:each) do
+      person = stub_model(Person)
+      Person.stub_chain(:current_candidates, :find_by_id).and_return(person)
+
+      candidate = stub_model(Candidate, person_id: 1)
+      Candidate.stub_chain(:find_by_id).and_return(candidate)
+
+      @challenge = stub_model(Challenge, id: 1, candidate_id: 1)
+    end
+
+    it "identifies a current challenge" do
+      expect(@challenge.is_current_challenge?).to equal(true)
+    end
+
+    it "identifies a noncurrent challenge" do
+      Person.stub_chain(:current_candidates, :find_by_id).and_return(nil)
+      expect(@challenge.is_current_challenge?).to equal(false)
+    end
+  end
 end
