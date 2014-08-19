4267b87167f2ed8bb880fa442e7a26a67c61c232
Kelvin Chou <kelvin@squareup.com>
2014-08-19 00:10:43 -0700

    Initial commit for transaction schema

---
+++ app/models/transaction.rb
@@ -0,0 +1,12 @@
+class Transaction < ActiveRecord::Base
+  belongs_to :company, inverse_of :transactions  
+
+  validates :company,
+    presence :true
+  validates :amount,
+    presence :true
+  validates :charge_id,
+    presence :true
+
+    
+end
---
+++ db/migrate/20140819043007_create_transactions.rb
@@ -0,0 +1,14 @@
+class CreateTransactions < ActiveRecord::Migration
+  def change
+    create_table :transactions do |t|
+      t.belongs_to :company, null: false
+      t.long :amount, null: false
+      t.string :charge_id, null: false
+      t.text :description
+
+      t.timestamps
+    end
+
+    add_index :transactions, :charge_id, unique: true
+  end
+end
