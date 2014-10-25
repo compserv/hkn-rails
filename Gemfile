a8870dc3ff2b7cfe83c5a00b0c29be1b9739321f
Joseph Hui <ctu@berkeley.edu>
2014-10-24 20:15:19 -0700

    CR comments

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
-ruby '2.1.3'
+ruby '2.1.2'
 
 gem 'activesupport'
 gem 'activerecord-session_store'
--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,6 @@ LDAP_SERVER_PORT = 389
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url => "https://auth-test.berkeley.edu/cas/"
+  :cas_base_url => "https://auth.berkeley.edu/cas/"
 )
 
