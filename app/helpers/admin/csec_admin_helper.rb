63a17b0b8abca2659db963fe222114e13c612f5a
Joseph Hui <ctu@berkeley.edu>
2015-03-29 22:06:10 -0700

    Improved course surveys upload

--- app/helpers/admin/csec_admin_helper.rb
+++ app/helpers/admin/csec_admin_helper.rb
@@ -2,206 +2,169 @@ module SurveyData
   class Importer
     require 'csv'
 
-    ##
-    # Import from CSV
-    ##
-    def self.import(type, file, commit=false, is_ta=nil)
+    QMATCH = /\A\d\. (.+)/
+    FREQUENCY_KEYS = ['1', '2', '3', '4', '5', '6', '7', 'N/A', 'Omit']
+
+    # Imports all of the course surveys from the specified file. Commits
+    # only if COMMIT is true.
+    def self.import(uh_FIXME, file, commit=false, is_ta=false)
+      is_ta = is_ta.nil? ? false : true
       results = { :errors => [], :info => [] }
-      result = []
-      last_row = ["none", 0]
-
-      results[:info] << "Save = #{!!commit}" if commit
-
+      rows = CSV.read(file.path)
       [:instructors, :klasses, :instructorships, :courses].each {|s| ActiveRecord::Base.connection.execute "ANALYZE #{s.to_s};"}
 
       begin
         ActiveRecord::Base.transaction do
-          state = :init
-          frequency_keys = {}
-          current = {}
-
-          CSV.foreach(file.path) do |row|
-            next if row.compact.empty?
-            last_row = [row.join(' '), last_row[1]+1] unless last_row[0].eql? row.join(' ')
-            # puts "Entering state #{state.to_s}"
-
-            case state
-            when :init
-              # Initialize vars that are cross-state, but not cross-klass
-              frequency_keys = []   # Ordered list of [1], [2], N/A, ...
-              frequencies = {}      # Frequencies that will go into SurveyAnswer
-              result = []           # For convenience, will be appended to results[:info] at :finish state
-              current = {:course     => {},
-                         :klass      => {},
-                         :instructor => {},
-                         :instructorship => {},
-                         :answers    => []
-                        }            # Cross-state state
-              state = :accept_klass
-              redo
-            when :accept_klass
-              # Row like
-              # ["EE 100-1 NIKNEJAD,ALI (prof)", "60 RESPONDENTS", nil, nil, nil, nil, nil, nil, nil, nil, nil, "FALL 2010", nil] 
-              
-              row.compact!
-              s = row.shift.upcase.split /\s+/
-
-              c = current[:course    ]  # course
-              k = current[:klass     ]  # klass
-              i = current[:instructor]  # instructor
-
-              # Course, klass
-              c[:abbr] = s.shift
-              c[:number], k[:section] = s.shift.split '-'   # 100-1 => [100, 1]
-              k[:semester] = Klass.semester_code_from_s(row.pop)
-
-              if dept = Department.find_by_nice_abbr(c[:abbr]) then
-                c.delete :abbr
-                c[:department_id] = dept.id
-              else  # no dept found
-                results[:errors] << "Couldn't find #{c[:abbr]} dept"
-                state = :error
-                redo
-              end
-
-              c.merge! Course.split_course_number(c.delete(:number))
-              course = Course.find_by_prefix_and_course_number_and_suffix_and_department_id(c[:prefix], c[:course_number], c[:suffix], c[:department_id]) || Course.new(c)
-
-              raise if k[:section].blank?
-              # REVELATION: currently, all TAs are globbed into a single section, and sections differentiate instructors.
-              # FIXME: make default section nil, instead of 0.
-              ### k[:section] = k[:section].blank? ? 0 : k[:section].to_i 
-              k[:section] = 0 unless course.course_number % 100 == 94
-              k[:section] = k[:section].to_i
-              k[:course_id] = course.id || 'OMGWTFBBQ'
-              klass = Klass.find_by(k) || Klass.new(k)
-              klass.course = course
-
-              # Instructor
-              i[:title] = (s.last =~ /\(\w+\)/) ? s.pop.to_s[1..-2] : 'PROF'  # (prof) => prof
-              i[:private] = true #false if i[:title] =~ /prof/i
-              i[:title] = {'PROF'=>'Professor', 'TA'=>'Teaching Assistant'}[i[:title]]
-              i[:last_name], i[:first_name] = s.join(' ').split(',').collect(&:strip).collect(&:titleize_with_dashes)
-              instructor = Instructor.find_by(['last_name LIKE ? AND first_name LIKE ?', i[:last_name], i[:first_name]]) || Instructor.new(i)
-
-              # Instructorship
-              instructorship = Instructorship.find_by_klass_id_and_instructor_id(klass.id, instructor.id) || Instructorship.new
-              #instructorship.instructor, instructorship.klass = instructor, klass  # NOTE this doesn't work, idk why
-              instructorship.ta = (is_ta.nil? ? !(i[:title] =~ /prof/i) : is_ta)
-              #instructorship.klass = klass
-
-              [:course, :klass, :instructor].each do |o|
-                m = binding.eval o.to_s
-                next if m && m.valid?
-                results[:errors] << "Invalid #{o.to_s}: #{m.inspect}"
-                state = :error
-              end
-              redo if state == :error
-
-              result << "#{course.course_abbr}-#{klass.section}, #{klass.proper_semester}"
-              [:course, :klass, :instructor, :instructorship].each do |s|
-                o = binding.eval s.to_s
-                pfix = (o.created_at.nil? ? '::NEW::' : 'existing')
-                result << ["#{pfix} #{s.to_s.capitalize}", [o.inspect]]
-                current[s] = o
-              end
-
-              raise unless [current[:course], current[:klass], current[:instructor], current[:instructorship]].all?
-
-              if commit then
-                [klass, course, instructor, instructorship].each do |o|
-                  instructorship.instructor, instructorship.klass = instructor, klass # wtf man
-                  next if o.save
-                  results[:errors] << "Failed to save #{o.inspect} because #{o.errors.inspect}"
-                  raise "save failed"
-                end
-              end
-
-              state = :frequencies
-
-            when :frequencies
-              row.compact!
-              next if row.first =~ /FREQUENCIES/i
-              row.each do |k|
-                case
-                when k =~ /\[(.+)\]/
-                  k = $1
-                  redo
-                when k.is_int? || k =~ /N\/A|Omit/
-                  frequency_keys.push k
-                else next  # ignore stats, these are recomputed
-                end # row case
-              end # row.map
-              # result << ["Read frequency keys:", frequency_keys]
-              state = :data
-
-            when :data
-              case
-              when row.first =~ /\A[A-Z ]+\z/        # e.g. CLASSROOM PRESENTATION
-              when row.first =~ /\A\d\. (.+)/    # question data
-                # qtext = $1.gsub(/[^a-zA-Z]/,' ')
-                qtext = $1
-                q = SurveyQuestion.find_by_text(qtext) || SurveyQuestion.search {keywords qtext}.results.first
-                q = nil if !q || !SurveyQuestion.exists?(q.id)
-                unless q
-                  results[:errors] << "Couldn't find survey question matching \"#{qtext}\"... check spelling and perhaps do a find+replace to correct it."
-                  state = :error
-                  redo
-                end
-                a = {}
-                a[:survey_question_id] = q.id
-                a[:instructorship_id] = current[:instructorship].id
-                row.shift # question text
-                a[:frequencies] = Hash[frequency_keys.zip frequency_keys.collect{row.shift.to_i}]
-                a[:frequencies] = ActiveSupport::JSON.encode a[:frequencies]
-                a = SurveyAnswer.find_by(a) || SurveyAnswer.new(a)
-
-                if commit
-                  if a.save
-                    a.order = current[:answers].length+1
-                    a.recompute_stats!  # only do for commit because new klasses will cause null constraint errors
-                  else
-                    results[:errors] << "Error with survey answer #{a.inspect}"
-                    state = :error
-                  end
-                end
-
-                current[:answers] << a
-              when row.first =~ /\A1 is a low rating/
-                # Example scoring
-              when row.first =~ /\AData processed/
-                # end of klass
-                result << ["Survey responses:", current[:answers]]
-                state = :finish
-                redo
-              else
-                state = :error
-                redo
-              end
-              state = :data   # process moar
-
-            when :finish
-              results[:info] << result.dup
-              state = :init
-
-            when :error
-              raise
-            end # case state
-          end # CSV.parse
-        end # transaction
-      rescue => e
-        results[:errors] << "Error #{e.inspect} parsing near line #{last_row[1]}: #{last_row[0]}"
-        results[:errors] << ["Stack trace:", e.backtrace]
-        #raise if Rails.env.development?
+          while true
+            data = self.next_survey(rows)
+            break if data.nil?
+            semester, dept_id, course_number, section, instructor, survey_answers = data
+            self.save_survey(semester, dept_id, course_number, section, instructor, survey_answers, is_ta, commit)
+          end
+        end
+      rescue ParseError => e
+        results[:errors] = e.message
+        return results
       end
 
+      results[:info] = ["Successfully imported course surveys."]
       return results
-    end # import
-  end # Importer
-end # SurveyData
+    end
+
+    # Returns all the relevant data for the next survey in ROWS, or nil
+    # if the file ends before then.
+    def self.next_survey(rows)
+      # ["EE 100-1 NIKNEJAD,ALI (prof)", "60 RESPONDENTS", nil, nil, nil, nil, nil, nil, nil, nil, nil, "FALL 2010", nil] 
+      survey_row = self.next_row(rows)
+      return nil if survey_row.nil?
+
+      survey_info = survey_row.compact
+      semester = Klass.semester_code_from_s(survey_info.pop) # fall 2010
+
+      course_info = survey_info.shift.downcase.split /\s+/
+      dept_abbr = course_info.shift # EE
+      course_number, section = course_info.shift.split '-' # 100-1 => [100, 1]
+      raise ParseError, "No section listed in: #{survey_info}" if section.blank?
+      section = section.to_i
+
+      instructor_name = course_info.shift #niknejad,ali
+      last_name, first_name = instructor_name.split(',').collect(&:titleize_with_dashes)
+      instructor = Instructor.find_by(['last_name LIKE ? AND first_name LIKE ?', last_name, first_name])
+      if instructor.nil? # We can create classes, but creating instructors leads to pain with typos
+        raise ParseError, "Instructor F:#{first_name}, L:#{last_name} does not exist in: #{survey_row}"
+      end
+
+      row = self.next_row(rows)
+      survey_answers = []
+      while not self.contains(row, "data processed")
+        #"1. Rate the overall teaching effectiveness of this instructor",1,7,22,27,57,30,12,,3,4.7,1.3,5
+        row_data = row.dup
+        qtext = row_data.shift
+        if qtext =~ QMATCH
+          qtext = qtext.match(QMATCH).captures[0]
+        else
+          raise ParseError, "Invalid survey question format: '#{qtext}' (expected '1. blah blah') in #{row}"
+        end
+
+        question = SurveyQuestion.find_by_text(qtext) || SurveyQuestion.search {keywords qtext}.results.first
+        raise ParseError, "Invalid survey question '#{qtext}' in #{row}" if question.nil?
+
+        frequencies = {}
+        FREQUENCY_KEYS.each do |key|
+          frequencies[key] = row_data.shift.to_i
+        end
+        frequencies = ActiveSupport::JSON.encode frequencies
+        survey_answers << [question.id, frequencies]
+        row = self.next_row(rows)
+      end
+
+      dept = Department.find_by_nice_abbr(dept_abbr)
+      if dept.nil?
+        raise ParseError, "Invalid department #{dept_abbr} in: #{survey_row}"
+      else
+        dept_id = dept.id
+      end
+
+      puts "Loaded #{instructor}"
+
+      return semester, dept_id, course_number, section, instructor, survey_answers
+    end
+
+    # Given some relevant data about a course survey, validates the survey,
+    # and saves it if COMMIT is true.
+    def self.save_survey(semester, dept_id, course_number, section, instructor, survey_answers, is_ta, commit)
+      prefix, course_number, suffix = Course.split_course_number(course_number, {hash: false})
+      course = Course.where({prefix: prefix, course_number: course_number, suffix: suffix, department_id: dept_id}).first || Course.new(c)
+      course_id = course.id
+
+      klass = Klass.find_by({:section => section, :course_id => course_id, :semester => semester}) || Klass.new(k)
+      klass.course = course
+
+      instructorship = Instructorship.find_by_klass_id_and_instructor_id(klass.id, instructor.id) || Instructorship.new
+      instructorship.ta, instructorship.instructor, instructorship.klass = is_ta, instructor, klass
+      puts "NEW INSTRUCTORSHIP #{instructorship.inspect} WITH #{is_ta}"
+
+      [course, klass, instructor, instructorship].each do |o|
+        if o.nil? or not o.valid?
+          raise ParseError, "Invalid object to save: #{o.inspect}"
+        end
+      end
+
+      return if not commit
+
+      [klass, course, instructor, instructorship].each do |o|
+        if not o.save
+          raise ParseError, "Save failed: #{o.inspect}"
+        end
+      end
+
+      survey_answers.each do |pair|
+        qid, frequencies = pair
+        a = SurveyAnswer.find_by({:survey_question_id => qid, :instructorship_id => instructorship.id}) || SurveyAnswer.new(a)
+        a.frequencies = frequencies
+        if a.save
+          a.order = current[:answers].length + 1
+          a.recompute_stats!  # only do for commit because new klasses will cause null constraint errors
+        else
+          raise ParseError, "Error with survey answer #{a.inspect}"
+        end
+      end
+    end
+
+    # Returns the next relevant row, or nil if the file ends before then.
+    def self.next_row(rows)
+      row = rows.shift
+      while row != nil and self.skippable(row)
+        row = rows.shift
+      end
+      return row
+    end
+
+    # Returns true if a row has no relevant content.
+    def self.skippable(row)
+      if self.contains(row, "data processed")
+        return false
+      elsif row.compact.length <= 1
+        return true
+      elsif self.contains(row, "omit")
+        return true
+      else
+        return false
+      end
+    end
+
+    def self.contains(row, text)
+      row = row.collect {|x| x.nil? ? "" : x}
+      return row.sum.downcase.include? text
+    end
+  end
+
+  class ParseError < StandardError
+  end
+end
 
 module Admin
- module CsecAdminHelper
-   include SurveyData
- end
+  module CsecAdminHelper
+    include SurveyData
+  end
 end
