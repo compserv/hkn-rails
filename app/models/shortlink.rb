225b76d89d4b7304c0b19b33f63598bb0b1261b4
alanyao <alanyao@hkn.eecs.berkeley.edu>
2016-02-09 23:15:12 -0800

    add comment

--- app/models/shortlink.rb
+++ app/models/shortlink.rb
@@ -1,6 +1,7 @@
 class Shortlink < ActiveRecord::Base
   validates :in_url, :out_url, :http_status, :person_id, presence: true
   validates :in_url, uniqueness: true
+  # Don't allow fake redirects of important files, even if the rails default behavior changes.
   validates_format_of :in_url, without: /\.|\//
   belongs_to :person
 end
