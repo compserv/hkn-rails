36492b76a6caa0ec7c1797f17341a61967ec4ddd
alanyao <alanyao@hkn.eecs.berkeley.edu>
2016-02-09 22:27:12 -0800

    shortlink manual exclude

--- app/models/shortlink.rb
+++ app/models/shortlink.rb
@@ -1,5 +1,6 @@
 class Shortlink < ActiveRecord::Base
-  validates :in_url, :out_url, :http_status, :person_id, :presence => true
-  validates :in_url, :uniqueness => true
+  validates :in_url, :out_url, :http_status, :person_id, presence: true
+  validates :in_url, uniqueness: true
+  validates_format_of :in_url, without: /\.|\//
   belongs_to :person
 end
