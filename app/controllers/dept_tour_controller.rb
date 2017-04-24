504bbde806c9132735e12761820c110b40ca25ea
Jason Perrin <jasonvperrin@gmail.com>
2017-04-24 03:37:45 -0700

    Restrict tours to dates HKN is open

--- app/controllers/dept_tour_controller.rb
+++ app/controllers/dept_tour_controller.rb
@@ -46,7 +46,10 @@ class DeptTourController < ApplicationController
   private
     def valid_date?(date)
       dt = Time.parse(date)
-      return (11..17).include?(dt.hour) && dt > Time.now
+      # Make sure to update this (along with the date/time restrictions in
+      # datetimepicker.js.erb to match only times and dates that HKN is open)
+      time_valid = (11..17).include?(dt.hour) && dt > DateTime.now
+      date_valid = dt <= DateTime.new(2017, 5, 12) or dt >= DateTime.new(2017, 8, 23)
     rescue ArgumentError
       # If this isn't something that can be parsed as a time, it's obviously invalid
       return false
