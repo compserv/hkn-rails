96eb10402ff77dc7e610d0d5ee43f3ff12760eca
Jason Perrin <jasonvperrin@gmail.com>
2017-05-06 02:46:03 -0700

    Limit datetime picker for department tours again
    Fixes the issues introduced in 2dddc15e8 by separating out the
    department tour request datetime picker from the regular event pickers.
    
    Also department tours are only available for next semester since it's
    almost finals week

--- app/controllers/dept_tour_controller.rb
+++ app/controllers/dept_tour_controller.rb
@@ -49,7 +49,7 @@ class DeptTourController < ApplicationController
       # Make sure to update this (along with the date/time restrictions in
       # datetimepicker.js.erb to match only times and dates that HKN is open)
       time_valid = (11..17).include?(dt.hour) && dt > DateTime.now
-      date_valid = dt <= DateTime.new(2017, 5, 12) or dt >= DateTime.new(2017, 8, 23)
+      date_valid = dt >= DateTime.new(2017, 8, 23) and dt <= DateTime.new(2017, 12, 8)
     rescue ArgumentError
       # If this isn't something that can be parsed as a time, it's obviously invalid
       return false

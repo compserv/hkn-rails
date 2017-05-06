compserv/hkn-rails

commit 2dddc15e8b0e30360ec87707b6ac55cf8402c436

Fix datetime picker for tutoring calendar
TODO: Fix this in the future to have separate ones for department tours
and for everything else so that the department tours can be restricted.

app/assets/javascripts/datetimepicker.js.erb
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   $(".datetimepicker").datetimepicker({
-    beforeShowDay: function(date) {
-      var td = date.getDay();
-      return [(date.getDay() != 0 && date.getDay() != 6), '', (td != 'Sat' && td != 'Sun') ? '' : 'only on weekday'];
-    },
+    // beforeShowDay: function(date) {
+    //   var td = date.getDay();
+    //   return [(date.getDay() != 0 && date.getDay() != 6), '', (td != 'Sat' && td != 'Sun') ? '' : 'only on weekday'];
+    // },
 
     // Make sure to change this as needed to only allow tours within dates and times that HKN is open
     // This goes along with the restrictions in dept_tour_controller.rb
     // Months in JS are strange, they are actually zero-indexed while everything else is not.
-    hourMin: 11,
-    hourMax: 16,
+    // hourMin: 11,
+    // hourMax: 16,
     minDate: 0,
     maxDate: new Date(2017, 4, 12),
 
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
     stepHour: 1,
     stepMinute: 5,
-    hourGrid: 1,
+    hourGrid: 6,
     minuteGrid: 15,
 
     showOn: "button",

commit 96eb10402ff77dc7e610d0d5ee43f3ff12760eca

Limit datetime picker for department tours again
Fixes the issues introduced in 2dddc15e8 by separating out the
department tour request datetime picker from the regular event pickers.

Also department tours are only available for next semester since it's
almost finals week

app/assets/javascripts/datetimepicker.js.erb
@@ -1,18 +1,49 @@
 $(document).ready(function() {
-  $(".datetimepicker").datetimepicker({
-    // beforeShowDay: function(date) {
-    //   var td = date.getDay();
-    //   return [(date.getDay() != 0 && date.getDay() != 6), '', (td != 'Sat' && td != 'Sun') ? '' : 'only on weekday'];
-    // },
+  // This one is used for department tour requests
+  $(".datetimepicker.depttourpicker").datetimepicker({
 
-    // Make sure to change this as needed to only allow tours within dates and times that HKN is open
+    // Only show weekdays, since tours are not offered on weekends
+    beforeShowDay: function(date) {
+      var td = date.getDay();
+      // Don't allow Saturday (6) or Sunday (0) for tours
+      var selectable = (td != 0 && td != 6);
+
+      // This must return [0: Whether this date is selectable or not,
+      //                   1: A CSS class name to use (or '' for the default,
+      //                   2: An optional popup tooltip for the date]
+      return [selectable, '', ''];
+    },
+
+    // Make sure to change these hours and dates as needed to only allow tours
+    // within dates and times that HKN is open.
     // This goes along with the restrictions in dept_tour_controller.rb
+    hourMin: 11,
+    hourMax: 16,
+    minTime: '11:00 am',
+    maxTime: '4:00 pm',
     // Months in JS are strange, they are actually zero-indexed while everything else is not.
-    // hourMin: 11,
-    // hourMax: 16,
-    minDate: 0,
-    maxDate: new Date(2017, 4, 12),
+    // This is actually August, but zero-indexing makes it 7
+    minDate: new Date(2017, 7, 23),
+    // This is actually December, but zero-indexing makes it 11
+    maxDate: new Date(2017, 11, 8),
 
+    dateFormat: 'yy-mm-dd',
+    timeFormat: 'hh:mm tt',
+
+    stepHour: 1,
+    stepMinute: 5,
+    hourGrid: 1,
+    minuteGrid: 15,
+
+    showOn: "button",
+    buttonImage: "<%= asset_path('icons/calendar.gif') %>",
+    buttonImageOnly: true,
+    buttonText: "Show calendar",
+  });
+
+
+  // This one is used for calendar events (website calendars)
+  $(".datetimepicker.eventpicker").datetimepicker({
     dateFormat: 'yy-mm-dd',
     timeFormat: 'hh:mm tt',
 

app/controllers/dept_tour_controller.rb
@@ -49,7 +49,7 @@ class DeptTourController < ApplicationController
       # Make sure to update this (along with the date/time restrictions in
       # datetimepicker.js.erb to match only times and dates that HKN is open)
       time_valid = (11..17).include?(dt.hour) && dt > DateTime.now
-      date_valid = dt <= DateTime.new(2017, 5, 12) or dt >= DateTime.new(2017, 8, 23)
+      date_valid = dt >= DateTime.new(2017, 8, 23) and dt <= DateTime.new(2017, 12, 8)
     rescue ArgumentError
       # If this isn't something that can be parsed as a time, it's obviously invalid
       return false

app/views/dept_tour/signup.html.erb
@@ -26,7 +26,7 @@
 
   <div class="field">
     <%= label_tag :date, "Desired date & time", class: 'required-field' %>
-    <%= text_field_tag :date, nil, class: "datetimepicker" %>
+    <%= text_field_tag :date, nil, class: "datetimepicker depttourpicker" %>
   </div>
 
   <div class="field">

app/views/dept_tour_requests/_form.html.erb
@@ -15,7 +15,7 @@
     <%= f.text_field :name %>
     <br/>
     <%= f.label :date, "Date & Time: "%>
-    <%= f.text_field :date, size: 20, class: "datetimepicker" %>
+    <%= f.text_field :date, size: 20, class: "datetimepicker depttourpicker" %>
     <br/>
     <%= f.label :contact, "Contact Email Address: "%>
     <%= f.text_field :contact %>

app/views/events/_form.html.haml
@@ -111,10 +111,10 @@
     = f.select :event_type_id, EventType.all.map{|x| [x.name, x.id]}
   .field
     = f.label :start_time
-    = f.text_field :start_time, size: 20, class: "datetimepicker"
+    = f.text_field :start_time, size: 20, class: "datetimepicker eventpicker"
   .field
     = f.label :end_time
-    = f.text_field :end_time, size: 20, class: "datetimepicker"
+    = f.text_field :end_time, size: 20, class: "datetimepicker eventpicker"
   - permission_options = options_for_select([['All', nil]]+['candplus', 'candidates', 'members', 'comms'].map{|name| g = Group.find_by_name(name); [g.description, g.id]}, @event.view_permission_group_id)
   .field
     = f.label :view_permission_group_id, 'View Permission'
@@ -152,10 +152,10 @@
             = number_field block_name, :rsvp_cap, value: block.rsvp_cap
           .field
             = label_tag :start_time, 'Start Time'
-            = text_field block_name, :start_time, size: 20, class: "datetimepicker", value: block.start_time
+            = text_field block_name, :start_time, size: 20, class: "datetimepicker eventpicker", value: block.start_time
           .field
             = label_tag :end_time, 'End Time'
-            = text_field block_name, :end_time, size: 20, class: "datetimepicker", value: block.end_time
+            = text_field block_name, :end_time, size: 20, class: "datetimepicker eventpicker", value: block.end_time
           %div
             - unless block.rsvps.empty?
               This block has RSVPs from the following people:
@@ -168,10 +168,10 @@
         = number_field :block, :rsvp_cap
       .field
         = label_tag :start_time, 'Start Time'
-        = text_field :block, :start_time, size: 20, class: "datetimepicker"
+        = text_field :block, :start_time, size: 20, class: "datetimepicker eventpicker"
       .field
         = label_tag :end_time, 'End Time'
-        = text_field :block, :end_time, size: 20, class: "datetimepicker"
+        = text_field :block, :end_time, size: 20, class: "datetimepicker eventpicker"
       = link_to 'Delete', '#', onclick: "$(this).parent().remove()"
   .field
     = f.label :need_transportation

app/views/indrel_events/_form.html.erb
@@ -8,7 +8,7 @@
 <% end %>
   <div class="field">
     <%= f.label :time %>
-    <%= f.text_field :time, size: 20, class: "datetimepicker" %> PST
+    <%= f.text_field :time, size: 20, class: "datetimepicker eventpicker" %> PST
   </div>
   <div class="field">
     <%= f.label :location %>