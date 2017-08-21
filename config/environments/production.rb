a761340f5f4454f889552aa47a0b52d066fc1410
Jason Perrin <jvperrin@yelp.com>
2017-08-21 12:55:29 -0600

    Use sendfile header for nginx instead of apache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ HknRails::Application.configure do
   config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  # For apache:
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
