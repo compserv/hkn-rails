e23ab3b1b89e9c1a7304751b426437c8a8b15af3
Jason Perrin <jasonvperrin@gmail.com>
2017-10-01 12:28:43 -0700

    Precompile eligibilities.css to fix vp admin error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,8 @@ Rails.application.config.assets.precompile += %w(
 
 # These are per-controller stylesheets or javascripts
 Rails.application.config.assets.precompile += %w(
-    candidates.css coursechart.css coursechart.js coursesurveys.css events.css
-    exams.css home.css.erb indrel.css people.css.erb static.scss
-    resume_books.css tutor.css.erb courseguide.js.erb coursesurveys.js.erb
-    tutor.js
+    candidates.css coursechart.css coursechart.js coursesurveys.css
+    eligibilities.css events.css exams.css home.css.erb indrel.css
+    people.css.erb static.scss resume_books.css tutor.css.erb
+    courseguide.js.erb coursesurveys.js.erb tutor.js
 )
