778cffa19aef25b6650174e68023d3f1eac6320d
Jason Perrin <jasonvperrin@gmail.com>
2017-12-09 20:25:16 -0800

    Use encrypted session cookies instead of db

--- db/schema.rb
+++ db/schema.rb
@@ -487,16 +487,6 @@ ActiveRecord::Schema.define(version: 20170306022053) do
     t.datetime "updated_at"
   end
 
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
-
   create_table "shortlinks", force: true do |t|
     t.string   "in_url"
     t.text     "out_url"
