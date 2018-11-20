545e6a38282ec7720f0e37f3ef8b0f250615d3b9
James Zhu <jameszhu@berkeley.edu>
2018-11-19 23:43:36 -0600

    Remove pg dependencies, add mariadb installs

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,12 +5,19 @@ update-locale LC_ALL=en_US.UTF-8
 source /etc/default/locale
 
 apt-get update
-apt-get install -y make g++
-apt-get install -y curl make openjdk-7-jre screen vim git
-apt-get install -y build-essential libxslt-dev libxml2-dev libmysqlclient-dev
+apt-get install -y
+    build-essential \
+    curl \
+    openjdk-7-jre \
+    screen \
+    vim \
+    git \
+    libxslt-dev \
+    libxml2-dev \
+    mariadb-client \
+    mariadb-server \
+    libmysqlclient-dev
 
-# Configure PostgreSQL
-# cp vagrant/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
-# su postgres -c "psql -c \"CREATE USER hkn_rails WITH PASSWORD 'hkn_rails' CREATEDB;\""
-# # systemctl restart postgresql
-# /etc/init.d/postgresql restart
+# Configure MySQL
+mysql -c "CREATE USER hkn_rails WITH PASSWORD 'hkn_rails' CREATEDB;"
+/etc/init.d/mysqld restart
