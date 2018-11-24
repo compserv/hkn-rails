3095c91f25c1c5c6be9f223cbc3806b4f49ab03f
James Zhu <jameszhu@berkeley.edu>
2018-11-24 00:36:39 -0600

    Fixup apt dependencies for jessie/stretch compat

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,6 +4,9 @@
 update-locale LC_ALL=en_US.UTF-8
 source /etc/default/locale
 
+# Add jessie-backports to /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
+
 apt-get update
 apt-get install -y \
     build-essential \
@@ -16,7 +19,7 @@ apt-get install -y \
     libxml2-dev \
     mariadb-client \
     mariadb-server \
-    libmysqlclient-dev
+    default-libmysqlclient-dev \
 
 # Configure MySQL
 mysql -e "GRANT ALL PRIVILEGES ON \`hkn\_rails\_%\`.* TO 'hkn_rails'@'localhost' IDENTIFIED BY 'hkn_rails';"
