7bf72b6beef5b8ffcec567b4d81b0fad170005d6
James Zhu <jameszhu@berkeley.edu>
2019-04-22 20:50:18 -0700

    Update prod deploy settings
    Updates prod Ruby (in RVM) to 2.5.1,
    swaps the uniocrn systemd tasks to point to hkn-rails.service.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -37,7 +37,7 @@ set :branch, 'master'
 # Feel free to add new variables to customise your setup.
 
 # RVM config
-set :rvm_ruby_version, '2.5.0'
+set :rvm_ruby_version, '2.5.1'
 
 
 # Custom SSH Options
