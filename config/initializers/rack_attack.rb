1f0b9b2eba8d99e6642ef92e3c9fb2411bb21f76
Oscar Chan <oscar4856@yahoo.com>
2022-04-23 19:14:17 -0700

    Rack Attack syntax fixup

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,10 +5,10 @@ class Rack::Attack
     if spammers.present?
         spammer_regexp = Regexp.union(spammers)
         Rack::Attack.blocklist('block spam ip') do |req|
-            req.ip ~= spammer_regexp
+            req.ip =~ spammer_regexp
         end
     end
-    attack_ban_retries  = ENV['RACK_ATTACK_RETRY'].try(:to_i) || 0
+    attack_ban_retries  = ENV['RACK_ATTACK_RETRY'].try(:to_i) || 1
     attack_ban_findtime = ENV['RACK_ATTACK_FIND_TIME'].try(:to_i) || 10
     attack_ban_bandtime = ENV['RACK_ATTACK_BAND_TIME'].try(:to_i) || 60
     if attack_ban_retries > 0
@@ -19,17 +19,17 @@ class Rack::Attack
        end
     end
     ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, start, finish, request_id, payload|
-      req         = payload[:request]
-      remote_ip   = req.env["HTTP_X_FORWARDED_FOR"].presence || req.env["REMOTE_ADDR"].presence
-      url         = req.env["REQUEST_URI"].presence
-      baned_time  = ENV['RACK_ATTACK_BAND_TIME'].try(:to_i)
-      if %i[throttle blocklist].include?(req.env['rack.attack.match_type'])
-        Rails.logger.info "[Rack::Attack][PostApp]" <<
+        req         = payload[:request]
+        remote_ip   = req.env["HTTP_X_FORWARDED_FOR"].presence || req.env["REMOTE_ADDR"].presence
+        url         = req.env["REQUEST_URI"].presence
+        baned_time  = ENV['RACK_ATTACK_BAND_TIME'].try(:to_i)
+        if %i[throttle blocklist].include?(req.env['rack.attack.match_type'])
+            Rails.logger.info "[Rack::Attack][PostApp]" <<
                       " remote_ip: \"#{remote_ip}\"," <<
                       " banned time: #{baned_time} seconds"
-       end
-     end
-    Rack::Attack.blocklisted_response = lambda do |env|
+        end
+    end    
+    Rack::Attack.blocklisted_responder = lambda do |env|
         [503, {}, ["Server Error\n"]]
     end
 end
